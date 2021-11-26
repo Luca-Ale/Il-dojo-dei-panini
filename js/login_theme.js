7aefaa14fbcc01aac1ce0beea211ab7efce85e45
Luca <luca.rengo@studio.unibo.it>
2021-11-26 21:24:25 +0100

    Add dark theme style and function in javascript.

---
+++ js/login_theme.js
@@ -0,0 +1,18 @@
+
+function swapTheme(){
+    let style = document.getElementById("theme-style");
+    let style_path = (style.href).substring((style.href).length-15, (style.href).length);
+
+    let icon_theme = document.getElementById("icon_theme");
+
+    console.log(style_path);
+    
+    if(style_path == "login_style.css"){
+        style.href = "../css/login_dark_theme_style.css";
+        icon_theme.src = "../icons/lightbulb-solid.svg";
+
+    } else {
+        style.href = "../css/login_style.css";
+        icon_theme.src = "../icons/lightbulb-regular.svg";
+    }
+}
