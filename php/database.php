0486760f58b74b34bb665fffe7c84731d1aee9b1
Luca <luca.rengo@studio.unibo.it>
2021-12-16 14:55:49 +0100

    Update database to include a bunch of new functions for the login and signup

--- php/database.php
+++ php/database.php
@@ -6,17 +6,8 @@ class DatabaseHelper{
         $this->db = new mysqli($servername, $username, $password, $dbname);
     }
 
-    public function insertNewCostumer($CF_CLIENTE, $nome, $cognome, $username, $mail, $password) {
-        $stmt = $this -> db -> prepare("insert into clienti values (?, ?, ?, ?, ?, ?)");
-        $stmt -> bind_param('ssssss', $CF_CLIENTE, $nome, $cognome, $username, $mail, $password);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
-
     public function getMenuByPriceOrder() {
-        $stmt = $this -> db -> prepare("select * from prodotti order by prezzo asc");
+        $stmt = $this -> db -> prepare("SELECT * FROM prodotti ORDER BY prezzo ASC");
         $stmt->execute();
         $result = $stmt->get_result();
 
@@ -24,7 +15,7 @@ class DatabaseHelper{
     }
 
     public function insertProductOnMenu($codice, $nome, $prezzo, $quantitaDisponibile, $ingredienti) {
-        $stmt = $this -> db -> prepare("insert into prodotti values (?, ?, ?, ?, ?)");
+        $stmt = $this -> db -> prepare("INSERT INTO prodotti VALUES (?, ?, ?, ?, ?)");
         $stmt -> bind_param('isiis', $codice, $nome, $prezzo, $quantitaDisponibile, $ingredienti);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -32,7 +23,7 @@ class DatabaseHelper{
     }
 
     public function deleteProductOnMenuByCode($codice) {
-        $stmt = $this -> db -> prepare("delete from prodotti where codice_prodotto=?");
+        $stmt = $this -> db -> prepare("DELETE FROM prodotti WHERE codice_prodotto = ?");
         $stmt->bind_param('i', $codice);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -40,9 +31,45 @@ class DatabaseHelper{
     }
 
     public function getShoppingCartProducts() {
-        $stmt = $this -> db -> prepare("SELECT * from carrello as c, prodotti as p where p.codice_prodotto = c.cod_prod;");
+        $stmt = $this -> db -> prepare("SELECT * FROM carrello AS c, prodotti AS p WHERE p.codice_prodotto = c.cod_prod;"); // serve il ; nella query ? Nelle altre non serve.
         $stmt->execute();
         $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function checkLogin($username, $password){
+        const $admin = "admin";
+        if(substr($admin, 0) === $username){
+            $stmt = $this->db->prepare("SELECT * FROM admin WHERE username = ? OR email = ? AND password = ?"); 
+            $stmt->bind_param("sss", $username, $username, $password); // metto username due volte perchè posso usare sia la username che l'email per loggare.
+            $stmt->execute();
+            $result = $stmt->get_result();
+            return $result->fetch_all(MYSQLI_ASSOC);
+            
+        } else {
+            
+            $stmt = $this->db->prepare("SELECT * FROM users WHERE username = ? OR email = ? AND password = ?"); 
+            $stmt->bind_param("sss", $username, $username, $password); // metto username due volte perchè posso usare sia la username che l'email per loggare.
+            $stmt->execute();
+            $result = $stmt->get_result();
+            return $result->fetch_all(MYSQLI_ASSOC);
+        }
+    }
+	
+	public function updatePassword($username, $email, $newPassword){
+        $stmt = $this->db->prepare("UPDATE users SET password = ? WHERE username = ? AND email = ?");
+        $stmt->bind_param("sss", $newPassword, $username, $email); 
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+	}
+
+    public function registerNewUser($username, $email, $password, $attivo) { //TODO: attivo forse non serve.
+        $stmt = $this->db->prepare("INSERT INTO users (ID, CF_cliente, nome, cognome, username, email, password, ATTIVO) VALUES (NULL, NULL, NULL, NULL, ?, ?, ?, ?)"); //TODO: CF_cliente, nome, cognome sono di default NULL in realtà.
+        $stmt->bind_param('sssi', $username, $email, $password, $attivo);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
         return $result->fetch_all(MYSQLI_ASSOC);
     }
     
