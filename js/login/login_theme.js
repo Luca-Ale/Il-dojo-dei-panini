2e633225f185ae05c1e14a6968ed637746a595a9
Luca <luca.rengo@studio.unibo.it>
2021-12-16 16:27:21 +0100

    Update login and signup themes scripts.

--- js/login/login_theme.js
+++ js/login/login_theme.js
@@ -1,17 +1,17 @@
 
 function swapTheme(){
-    let style = document.getElementById("theme-style");
-    let style_path = (style.href).substring((style.href).length-15, (style.href).length);
+    let style = document.getElementById("theme-style-login");
+    let style_path = style.getAttribute('href');
 
     let icon_theme = document.getElementById("icon_theme");
     
-    if(style_path == "login_style.css"){
-        style.href = "../css/login_dark_theme_style.css";
-        icon_theme.src = "../icons/theme/lightbulb-solid.svg";
+    if(style_path == "../css/login_style.css"){
+        style.setAttribute('href', '../css/login_dark_theme_style.css');
+        icon_theme.setAttribute('src', '../icons/theme/lightbulb-solid.svg');
 
     } else {
-        style.href = "../css/login_style.css";
-        icon_theme.src = "../icons/theme/lightbulb-regular.svg";
+        style.setAttribute('href', '../css/login_style.css');
+        icon_theme.setAttribute('src', '../icons/theme/lightbulb-regular.svg');
         
     }
 }
--- js/signup/signup_theme.js
+++ js/signup/signup_theme.js
@@ -1,16 +1,16 @@
 
 function swapTheme(){
-    let style = document.getElementById("theme-style");
-    let style_path = (style.href).substring((style.href).length-16, (style.href).length);
+    let style = document.getElementById("theme-style-signup");
+    let style_path = style.getAttribute('href');
 
     let icon_theme = document.getElementById("icon_theme");
     
-    if(style_path == "signup_style.css"){
-        style.href = "../css/signup_dark_theme_style.css";
-        icon_theme.src = "../icons/theme/lightbulb-solid.svg";
+    if(style_path == "../css/signup_style.css"){
+        style.setAttribute('href', '../css/signup_dark_theme_style.css');
+        icon_theme.setAttribute('src', '../icons/theme/lightbulb-solid.svg');
 
     } else {
-        style.href = "../css/signup_style.css";
-        icon_theme.src = "../icons/theme/lightbulb-regular.svg";
+        style.setAttribute('href', '../css/signup_style.css');
+        icon_theme.setAttribute('src', '../icons/theme/lightbulb-regular.svg');
     }
 }
