c85feb37799f6f452407c9fb3206eecc7be2a624
Luca <luca.rengo@studio.unibo.it>
2021-12-17 14:57:18 +0100

    Add login php file to let user login

---
+++ php/login.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'bootstrap.php';
+
+if(isset($_POST["username"]) && isset($_POST["password"])){
+    $login_result = $dbh->login($_POST["username"], $_POST["password"]);
+    
+    if(count($login_result) == 0){
+        //Login fallito
+        $templateParams["errorelogin"] = "Errore! Controllare username o password!";
+    }
+
+}
+
+if(isUserLoggedIn()){
+    $templateParams["titolo"] = "Il Dojo dei Panini | Benvenuto Utente";
+    $templateParams["nome"] = "index.php"; //TODO: modificare
+} else {
+    $templateParams["titolo"] = "Il Dojo dei Panini | Login";
+    $templateParams["nome"] = "login-page.php";
+}
+
+require 'login-page.php';
+?>
