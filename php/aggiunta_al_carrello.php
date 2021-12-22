e95b19710ae9b8f26afaf578c3896edb6815be56
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2021-12-22 12:39:17 +0100

    add working links

--- php/aggiunta_al_carrello.php
+++ php/aggiunta_al_carrello.php
@@ -1,13 +1,20 @@
 <?php
 require_once 'bootstrap.php';
-$query = $dbh->isProductAlreadyOnShoppingCart($_GET["id"], $_SESSION["UserID"]);
-
-if (!empty($query)) {
-    //Il prodotto è già presente nel carrello, dunque va fatto solo l'update della quantità
-    $dbh->addQuantityToShoppingCart($_GET["id"], $_SESSION["UserID"]);
+if(isUserLoggedIn()) {
+    $query = $dbh->isProductAlreadyOnShoppingCart($_GET["id"], $_SESSION["UserID"]);
+    if (!empty($query)) {
+        //Il prodotto è già presente nel carrello, dunque va fatto solo l'update della quantità
+        $dbh->addQuantityToShoppingCart($_GET["id"], $_SESSION["UserID"]);
+    } else {
+        //Il prodotto non è nel carrello, va aggiunto da 0
+        $dbh->addToShoppingCart($_GET["id"], $_SESSION["UserID"], 1);
+    }
+    header("Refresh:0; url=shop.php");
 } else {
-    //Il prodotto non è nel carrello, va aggiunto da 0
-    $dbh->addToShoppingCart($_GET["id"], $_SESSION["UserID"], 1);
+    header("Refresh:0; url=login.php");
 }
-header("Refresh:0; url=shop.php");
+
+
+
+
 ?>
