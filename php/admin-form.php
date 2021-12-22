9eb4a80b567562a609ea7965be46e9aef7b28d4c
Luca <luca.rengo@studio.unibo.it>
2021-12-22 15:46:29 +0100

    Add login-home-admin page and admin-form

---
+++ php/admin-form.php
@@ -0,0 +1,43 @@
+<?php 
+$prodotto = $templateParams["prodotto"]; 
+$azione = getAction($templateParams["azione"])
+?>
+<form action="processa-prodotto.php" method="POST" enctype="multipart/form-data">
+    <h2>Gestisci Prodotto</h2>
+    <?php if($prodotto==null): ?>
+    <p>Prodotto non trovato</p>
+    <?php else: ?>
+    <ul>
+        <li>
+            <label for="nomeprodotto">Nome:</label><input type="text" id="nomeprodotto" name="nomeprodotto" value="<?php echo $prodotto["nome"]; ?>" /> 
+        </li>
+        <li>
+            <label for="prezzoprodotto">Prezzo:</label><input type="text" id="prezzoprodotto" name="prezzoprodotto" value="<?php echo $prodotto["prezzo"]; ?>" /> 
+        </li>
+        <li>
+            <label for="quantitaprodotto">Quantità:</label><input type="text" id="quantitaprodotto" name="quantitaprodotto"><?php echo $prodotto["quantita_disponibile"]; ?>
+        </li>
+        <li>
+            <label for="ingredientiprodotto">Ingredienti:</label><textarea id="ingredientiprodotto" name="ingredientiprodotto"><?php echo $prodotto["ingredienti"]; ?></textarea>
+        </li>
+        <li>
+            <?php if($templateParams["azione"]!=3): ?>
+            <label for="imgprodotto">Immagine Prodotto</label><input type="file" name="imgprodotto" id="imgprodotto" />
+            <?php endif; ?>
+            <?php if($templateParams["azione"]!=1): ?>
+            <img src="<?php echo IMG_DIR.$prodotto["img"]; ?>" alt="" />
+            <?php endif; ?>
+        </li>
+        <li>
+            <input type="submit" name="submit" value="<?php echo $azione; ?> Prodotto" />
+            <a href="login.php">Annulla</a>
+        </li>
+    </ul>
+        <?php if($templateParams["azione"]!=1): ?>
+        <input type="hidden" name="idprodotto" value="<?php echo $prodotto["codice_prodotto"]; ?>" />
+        <input type="hidden" name="oldimg" value="<?php echo $prodotto["img"]; ?>" />
+        <?php endif;?>
+
+        <input type="hidden" name="action" value="<?php echo $templateParams["azione"]; ?>" />
+    <?php endif;?>
+</form>
---
+++ php/login-home-admin.php
@@ -0,0 +1,22 @@
+<section>
+    <h2>Prodotti</h2>
+    <?php if(isset($templateParams["formmsg"])):?>
+    <p><?php echo $templateParams["formmsg"]; ?></p>
+    <?php endif; ?>
+    <a href="gestisci-prodotti.php?action=1">Inserisci Prodotto</a>
+    <table>
+        <tr>
+            <th>Titolo</th><th>Immagine</th><th>Azione</th>
+        </tr>
+        <?php foreach($templateParams["prodotti"] as $prodotto): ?>
+        <tr>
+            <td><?php echo $prodotto["nomep"]; ?></td>
+            <td><img src="<?php echo IMG_DIR.$prodotto["img"]; ?>" alt="" /></td>
+            <td>
+                <a href="gestisci-prodotti.php?action=2&id=<?php echo $prodotto["codice_prodotto"]; ?>">Modifica</a>
+                <a href="gestisci-prodotti.php?action=3&id=<?php echo $prodotto["codice_prodotto"]; ?>">Cancella</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</section>
