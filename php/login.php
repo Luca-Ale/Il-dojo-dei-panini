cff21133c45531da94ad9bbee00128990af3ffa9
Luca <luca.rengo@studio.unibo.it>
2021-12-22 23:16:00 +0100

    Update add titolo to login page

--- php/login.php
+++ php/login.php
@@ -27,6 +27,7 @@ if(isUserLoggedIn()){
 } else {
     if(isAdminLoggedIn()){
         $templateParams["nome"] = "login-home-admin.php";
+        $templateParams["titolo"] = "Il Dojo dei Panini | Prodotti";
         $templateParams["prodotti"] = $dbh->getAllProducts();
         if(isset($_GET["formmsg"])){
             $templateParams["formmsg"] = $_GET["formmsg"];
