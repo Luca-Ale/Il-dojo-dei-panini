e584c58350feec46f5476717014a0156a4a41929
Luca <luca.rengo@studio.unibo.it>
2021-12-24 17:34:16 +0100

    Update buttons and links

--- php/template/login-home-admin.php
+++ php/template/login-home-admin.php
@@ -3,18 +3,19 @@
     <?php if(isset($templateParams["formmsg"])):?>
     <p><?php echo $templateParams["formmsg"]; ?></p>
     <?php endif; ?>
-    <a href="gestisci-prodotti.php?action=1">Inserisci Prodotto</a>
+    <a href="gestisci-prodotti.php?action=1" class="btn btn-success">Inserisci Prodotto</a>
+    <a href="login-admin-messaggi.php" class="btn btn-success">Area Notifiche</a>
     <table>
         <tr>
-            <th>Titolo</th><th>Immagine</th><th>Azione</th>
+            <th>Nome</th><th>Immagine</th><th>Azione</th>
         </tr>
         <?php foreach($templateParams["prodotti"] as $prodotto): ?>
         <tr>
             <td><?php echo $prodotto["nome"]; ?></td>
             <td><img width="120" height="120" src="<?php echo IMG_DIR.$prodotto["img"]; ?>" alt="" /></td>
             <td>
-                <a href="gestisci-prodotti.php?action=2&id=<?php echo $prodotto["codice_prodotto"]; ?>">Modifica</a>
-                <a href="gestisci-prodotti.php?action=3&id=<?php echo $prodotto["codice_prodotto"]; ?>">Cancella</a>
+                <a class="btn btn-success" href="gestisci-prodotti.php?action=2&id=<?php echo $prodotto["codice_prodotto"]; ?>">Modifica</a>
+                <a class="btn btn-danger" href="gestisci-prodotti.php?action=3&id=<?php echo $prodotto["codice_prodotto"]; ?>">Cancella</a>
             </td>
         </tr>
         <?php endforeach; ?>
