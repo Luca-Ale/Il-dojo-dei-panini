e367798a2f4a21195d029a4c57b66dc33af5ce1f
Luca <luca.rengo@studio.unibo.it>
2021-12-24 23:00:38 +0100

    Update to add € symbol in the message.

--- php/processa-prodotto.php
+++ php/processa-prodotto.php
@@ -19,7 +19,7 @@ if($_POST["action"] == 1){
     }
     
     $oggetto = "Inserimento";
-	$testo_messaggio = "Il prodotto: ". "$nomeProdotto". " al prezzo di ". "$prezzoProdotto, alla quantità: "."$quantitaProdotto". " con gli ingredienti: "."$ingredientiProdotto è stato inserito correttamente! dal admin ". $_SESSION["AdminID"] . " : " . $_SESSION["username"];
+	$testo_messaggio = "Il prodotto: ". "$nomeProdotto". " al prezzo di ". "$prezzoProdotto €, alla quantità: "."$quantitaProdotto". " con gli ingredienti: "."$ingredientiProdotto è stato inserito correttamente! dal admin ". $_SESSION["AdminID"] . " : " . $_SESSION["username"];
 	$dbh->insertNewMessageForAdmin($oggetto, $testo_messaggio);
 
     header("location: login.php?formmsg=".$msg);
@@ -46,7 +46,7 @@ if($_POST["action"] == 2){
     $dbh->updateProduct($codiceProdotto, $nomeProdotto, $prezzoProdotto, $quantitaProdotto, $ingredientiProdotto,$imgProdotto);
 
     $oggetto = "Modifica";
-	$testo_messaggio = "Il prodotto: ". "$nomeProdotto". " al prezzo di ". "$prezzoProdotto, alla quantità: "."$quantitaProdotto". " con gli ingredienti: "."$ingredientiProdotto è stato modificato correttamente! dal admin ". $_SESSION["AdminID"] . " : " . $_SESSION["username"];
+	$testo_messaggio = "Il prodotto: ". "$nomeProdotto". " al prezzo di ". "$prezzoProdotto €, alla quantità: "."$quantitaProdotto". " con gli ingredienti: "."$ingredientiProdotto è stato modificato correttamente! dal admin ". $_SESSION["AdminID"] . " : " . $_SESSION["username"];
 	$dbh->insertNewMessageForAdmin($oggetto, $testo_messaggio);
 
     $msg = "Modifica completata correttamente!";
@@ -60,7 +60,7 @@ if($_POST["action"] == 3){
     $dbh->deleteProductOnMenuByCode($codiceProdotto);
 
     $oggetto = "Rimozione";
-	$testo_messaggio = "Il prodotto: ". "$codiceProdotto ". htmlspecialchars($_POST["nome"]) . " di prezzo " . htmlspecialchars($_POST["prezzo"]) . ", quantità: " . htmlspecialchars($_POST["quantita_disponibile"]) . " ed ingredienti: " . htmlspecialchars($_POST["ingredienti"]) . " è stato cancellato correttamente dall'admin " . $_SESSION["AdminID"] . " : " . $_SESSION["username"];
+	$testo_messaggio = "Il prodotto: ". "$codiceProdotto ". htmlspecialchars($_POST["nome"]) . " di prezzo " . htmlspecialchars($_POST["prezzo"]) . " €, quantità: " . htmlspecialchars($_POST["quantita_disponibile"]) . " ed ingredienti: " . htmlspecialchars($_POST["ingredienti"]) . " è stato cancellato correttamente dall'admin " . $_SESSION["AdminID"] . " : " . $_SESSION["username"];
 	$dbh->insertNewMessageForAdmin($oggetto, $testo_messaggio);
     
     $msg = "Cancellazione completata correttamente!";
