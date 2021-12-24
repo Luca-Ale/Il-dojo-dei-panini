1d7b2c57ca84bc0fcc785ccd95f565a024f458e8
Luca <luca.rengo@studio.unibo.it>
2021-12-24 22:27:29 +0100

    Update index and add index page template

--- php/index.php
+++ php/index.php
@@ -6,5 +6,6 @@ $templateParams["titolo"] = "Il dojo dei panini | Home";
 $templateParams["nome"] = "index-tmplt.php";
 //$templateParams["product"] = $dbh->getShoppingCartProducts();
 
-require 'template/base.php';
+//require 'template/base.php'; // Il base ha il suo stile, mentre l'index ha un altro stile, inoltre ha anche un js per cambiare il tema.
+require "template/index-page-tmplt.php";
 ?>
