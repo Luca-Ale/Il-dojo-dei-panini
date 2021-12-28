b77288f2bd6628b026cc22c2e807c0519349d962
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2021-12-28 17:34:26 +0100

    renew template

--- db/create_database_structure_and_data.sql
+++ db/create_database_structure_and_data.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Creato il: Dic 24, 2021 alle 17:35
--- Versione del server: 10.4.21-MariaDB
--- Versione PHP: 8.0.12
+-- Host: 127.0.0.1
+-- Creato il: Dic 28, 2021 alle 17:33
+-- Versione del server: 10.4.22-MariaDB
+-- Versione PHP: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -79,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `messaggi` (
   `oggetto` varchar(120) COLLATE utf8_unicode_ci NOT NULL,
   `testo` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`codice_messaggio`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -106,7 +106,10 @@ INSERT INTO `notifiche` (`codice_notifica`, `oggetto`, `testo`, `codice_ordine`)
 (3, 'Conferma avvenuto ordine', 'Signor george1984, la confermiamo che in data 2021-12-24 11:29:41 lei ha effettuato l\'ordine12 in cui ha acquistato i prodotti:  patatine fritte, katanaburger, Cola, buona giornata', 12),
 (4, 'Conferma avvenuto ordine', 'Signor george1984, la confermiamo che in data 2021-12-24 11:33:04 lei ha effettuato l\'ordine13 in cui ha acquistato i prodotti:  Cola, katanaburger, samurai treccia, PANINOZZO, il quale costo totale è:28 buona giornata', 13),
 (5, 'Conferma avvenuto ordine', 'Signor george1984, la confermiamo che in data 2021-12-24 11:33:56 lei ha effettuato l\'ordine 14 in cui ha acquistato i prodotti:  Cola, samurai treccia, PANINOZZO, il quale costo totale è: 20€ buona giornata', 14),
-(6, 'Conferma avvenuto ordine', 'Signor george1984, la confermiamo che in data 2021-12-24 16:19:43 lei ha effettuato l\'ordine 15 in cui ha acquistato i prodotti:  samurai treccia, il quale costo totale Ã¨: 8â‚¬, il delivery partirÃ  fra 15 minuti, buona giornata.', 15);
+(6, 'Conferma avvenuto ordine', 'Signor george1984, la confermiamo che in data 2021-12-24 12:03:29 lei ha effettuato l\'ordine 15 in cui ha acquistato i prodotti:  Cola, katanaburger, il quale costo totale è: 10€ buona giornata.', 15),
+(7, 'Conferma avvenuto ordine', 'Signor george1984, la confermiamo che in data 2021-12-24 12:03:40 lei ha effettuato l\'ordine 16 in cui ha acquistato i prodotti:  Cola, patatine fritte, katanaburger, il quale costo totale è: 14€ buona giornata.', 16),
+(8, 'Conferma avvenuto ordine', 'Signor george1984, la confermiamo che in data 2021-12-27 17:16:05 lei ha effettuato l\'ordine 17 in cui ha acquistato i prodotti:  Cola, patatine fritte, Pizza, katanaburger, PANINOZZO, il quale costo totale è: 34€, il delivery partirà fra 15 minuti, buona giornata.', 17),
+(9, 'Conferma avvenuto ordine', 'Signor spera_carmela, la confermiamo che in data 2021-12-28 15:30:13 lei ha effettuato l\'ordine 18 in cui ha acquistato i prodotti:  Cola, patatine fritte, il quale costo totale è: 6€, il delivery partirà fra 15 minuti, buona giornata.', 18);
 
 -- --------------------------------------------------------
 
@@ -141,7 +144,10 @@ INSERT INTO `ordini` (`codice_ordine`, `UserID`, `DataOra`) VALUES
 (12, 5, '2021-12-24 10:29:41'),
 (13, 5, '2021-12-24 10:33:04'),
 (14, 5, '2021-12-24 10:33:56'),
-(15, 5, '2021-12-24 15:19:43');
+(15, 5, '2021-12-24 11:03:29'),
+(16, 5, '2021-12-24 11:03:40'),
+(17, 5, '2021-12-27 16:16:05'),
+(18, 4, '2021-12-28 14:30:13');
 
 -- --------------------------------------------------------
 
@@ -194,7 +200,18 @@ INSERT INTO `prod-ordine` (`codice_prodotto`, `codice_ordine`, `quantita_ordinat
 (1, 14, 1),
 (2, 14, 1),
 (6, 14, 1),
-(2, 15, 1);
+(1, 15, 1),
+(5, 15, 1),
+(1, 16, 1),
+(3, 16, 1),
+(5, 16, 1),
+(1, 17, 1),
+(3, 17, 1),
+(4, 17, 2),
+(5, 17, 1),
+(6, 17, 1),
+(1, 18, 1),
+(3, 18, 1);
 
 -- --------------------------------------------------------
 
@@ -210,19 +227,19 @@ CREATE TABLE IF NOT EXISTS `prodotti` (
   `ingredienti` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `img` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`codice_prodotto`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dump dei dati per la tabella `prodotti`
 --
 
 INSERT INTO `prodotti` (`codice_prodotto`, `nome`, `prezzo`, `quantita_disponibile`, `ingredienti`, `img`) VALUES
-(1, 'Cola', 2, 300, 'segreto', 'cocacola.jpg'),
-(2, 'samurai treccia', 10, 100, 'pane(treccia), hamburger, melanzane gratinate, salsa samurai', 'panino_con_spalla.jpg'),
-(3, 'patatine fritte', 4, 200, 'patate, olio di semi, sale, pepe', 'patatinefritte.jpg'),
-(4, 'Pizza', 5, 200, 'Impasto con farina 00, mozzarella, pomodoro, basilico, olio extravergine d\'oliva', 'pizze_classiche_gourmet.jpg\r\n'),
-(5, 'katanaburger', 8, 5, 'pane giapponese, hamburger, mozzarella di bufala, salsa alla mortadella, rucola, cetriolini', 'panino_gourmet.png\r\n'),
-(6, 'PANINOZZO', 10, 200, 'Panino con semola, carne di chianina, bacon croccante, guanciale, pomodoro fresco, insalata, salsa d', 'toasts.jpg');
+(1, 'Cola', 2, 1, 'segreto', 'cocacola.jpg'),
+(2, 'samurai treccia', 8, 100, 'pane(treccia), hamburger, melanzane gratinate, salsa samurai', 'panino_con_spalla.jpg'),
+(3, 'patatine fritte', 4, 20, 'patate, olio di semi, sale, pepe', 'patatinefritte.jpg'),
+(4, 'Pizza', 5, 124, 'Impasto con farina 00, mozzarella, pomodoro, basilico, olio extravergine d\'oliva', 'pizze_classiche_gourmet.jpg\r\n'),
+(5, 'katanaburger', 8, 10, 'pane giapponese, hamburger, mozzarella di bufala, salsa alla mortadella, rucola, cetriolini', 'panino_gourmet.png\r\n'),
+(6, 'PANINOZZO', 10, 194, 'Panino con semola, carne di chianina, bacon croccante, guanciale, pomodoro fresco, insalata, salsa d', 'toasts.jpg');
 
 -- --------------------------------------------------------
 
@@ -260,7 +277,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`UserID`, `username`, `email`, `password`, `attivo`) VALUES
-(1, 'tom', 'tom22@gmail.com', 'tomEjerry11', 1),
+(1, 'tom', 'tom22@gmail.com', 'tom_e_jerry11', 1),
 (2, 'camaleonte', 'carlettorossimbaldi@gmail.com', '1234', 1),
 (3, 'carmiSpii33', 'carmelospyder@gmail.com', 'password', 1),
 (4, 'spera_carmela', 'carmelasperanza@gmail.com', 'caramellagommosa', 1),
