0e5ea20307ae3c7de850699f592865341a520d1f
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2021-12-28 17:28:02 +0100

    finito col magazzino (effetto wow 1)

--- php/svuota_carrello.php
+++ php/svuota_carrello.php
@@ -1,5 +1,12 @@
 <?php 
 require_once 'bootstrap.php';
+$products = $dbh->getShoppingCartProducts($_SESSION["UserID"]);
+
+foreach($products as $prod) {
+    $shoppingCartProductQuantity = $dbh->getProductByIdFromShoppingCart($prod["cod_prodotto"], $_SESSION["UserID"])[0]["quantita"];
+    $dbh->setProductQuantity($shoppingCartProductQuantity, $prod["cod_prodotto"]);
+} 
 $dbh->deleteAllFromShoppingCart($_SESSION["UserID"]);
+
 header("Refresh:0; url=carrello.php");
 ?>
