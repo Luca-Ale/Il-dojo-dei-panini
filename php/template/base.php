48d6d67afb8df949caece56011a47f463f927ecc
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2021-12-28 18:19:19 +0100

    put some order on html pages

--- php/template/base.php
+++ php/template/base.php
@@ -16,7 +16,7 @@
             endforeach;
          endif;
         ?>
-
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <title><?php echo $templateParams["titolo"];?></title>
     </head>
     <body class="d-flex flex-column min-vh-100">
@@ -139,5 +139,4 @@
 
         </footer>    
     </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </html>
