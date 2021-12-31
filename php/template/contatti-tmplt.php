2a79cc9afa42e8d8ab7a15d98f052b125b0b7689
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2021-12-31 17:42:47 +0100

    modify contatti

--- php/template/contatti-tmplt.php
+++ php/template/contatti-tmplt.php
@@ -1,32 +1,30 @@
             <section>
-                <h1>Contatti</h1>
-                <ul>
-                    <li>
-                        PDF : Scarica qui il menù
+                <h1 class=" font-monospace  fw-bold text-center">Contatti</h1>
+                <ul class="list-group font-monospace fs-3 fw-bold list-group-flush">
+                    <li class="list-group-item">
+                        cellulare : 333666999
                     </li>
-                    <li>
-                        QR : Inquadra il codice qr(quello a sinistra) per visualizzare il menù.
-                    </li>
-                    <li>
+                    <li class="list-group-item">
                         Telefono : 0541385924
                     </li>
-                    <li>
+                    <li class="list-group-item">
                         Telegram : dojo-dei-panini
                     </li>
-                    <li>
+                    <li class="list-group-item">
                         Mail : Inviaci una mail a ildojodeipanini@gmail.com
-                    </li>
-                    <li>
-                        Donazione : IBAN : TD54G8G4GDA13A13486
+                    </li> 
+                    <li class="list-group-item">
+                        IBAN : TD54G8G4GDA13A13486
                     </li>
                 </ul>
             </section>
             <article>
-                <h2>Descrizione fondazione benefica</h2>
-                <p>
+                <h2 class=" font-monospace  fw-bold text-center">Descrizione fondazione benefica</h2>
+                <p class="list-group font-monospace fs-3">
                     Il nostro ristorante da tempo collabora con la società benefica save-a-kid.
                     La società, attraverso la raccolta di fondi, offre delle dimore e borse di studio ai bambini che appartengono al terzo mondo.
                     In caso si voglia collaborare occorre inviare un bonifico all'iban indicato in questa pagina.
                 </p>
-                <p>Con affetto da Alessandro e Luca(i proprietari)</p>
+                <p class="list-group font-monospace fs-3">Con affetto da Alessandro e Luca(i proprietari)</p>
             </article>
+          </section>
