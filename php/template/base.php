47209fc5cebdace934c6165108eb3902c7735a8a
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2022-01-02 17:44:09 +0100

    add title

--- php/template/base.php
+++ php/template/base.php
@@ -17,7 +17,7 @@
          endif;
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        <title><?php echo $templateParams["titolo"];?></title>
+        <title>Title: <?php echo $templateParams["titolo"];?></title>
     </head>
     <body class="d-flex flex-column min-vh-100">
         <header>
