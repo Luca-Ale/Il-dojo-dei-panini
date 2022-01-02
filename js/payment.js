c375e7f3567b30ef30757be52f2d7ee6211c478e
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2022-01-02 17:30:35 +0100

    fix stuff a simple thing in form

--- js/payment.js
+++ js/payment.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
         <form id="ciao" class="form-group font-monospace fw-bold fs-3" action="POST">
         <label for="nome" class="form-label">Inserire il nome dell'intestatario</label><input type="text" name="nome" id="nome" class="form-control">
         <label for="cognome" class="form-label">Inserire il cognome dell'intestatario</label><input type="text" name="cognome" id="cognome" class="form-control"> 
-        <label for="numero_carta" class="form-label">Inserire il numero della carta</label><input type="text" name="numero_carta" id="numero_carta" class="form-control">
+        <label for="numero_carta" class="form-label">Inserire il numero della carta</label><input type="number" name="numero_carta" id="numero_carta" class="form-control">
         <label class="form-label">Selezionare un metodo di pagamento</label>
         <select class="form-control form-control-lg mb-3">
             <option value="credito">Carta di credito</option>
