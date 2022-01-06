a2cdcb4387c46d00143858c5501e9e565aadadda
Luca <luca.rengo@studio.unibo.it>
2022-01-06 15:47:37 +0100

    Fix little issue

--- php/forgot-password.php
+++ php/forgot-password.php
@@ -3,7 +3,7 @@ require_once 'bootstrap.php';
 
 if(isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm-password"])){
     if($_POST["password"] == $_POST["confirm-password"]){
-        $result_update = $dbh->updatePassword($_POST["username"], $_POST["email"] && $_POST["password"]);
+        $result_update = $dbh->updatePassword($_POST["username"], $_POST["email"], $_POST["password"]);
         if($result_update == false){
             $templateParams["msgforgotpassword"] = "Errore nei dati.";
         } else {
