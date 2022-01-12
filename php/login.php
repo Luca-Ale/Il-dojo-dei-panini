9b3cf229868984e2d5e2f2a827f23acf67472f76
Luca <luca.rengo@studio.unibo.it>
2022-01-12 11:57:07 +0100

    Partially fix issue

--- php/login.php
+++ php/login.php
@@ -23,5 +23,13 @@ if(isset($_POST["username"]) && isset($_POST["password"])){
 
 }
 
+if(isUserLoggedIn()){
+	header("Refresh:0; url=carrello.php");
+} else {
+	if(isAdminLoggedIn()){
+		header("Refresh:0; url=login-admin.php");
+	}
+}
+
 require 'login-page.php';
 ?>
